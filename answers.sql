cf8cdb5633d38bfe4116fcbc2456e90942adc398
Clement Wambua <109575147+Clementwa0@users.noreply.github.com>
2025-04-29 11:14:06 +0300

    completed Week 7 Assignment

---
+++ answers.sql
@@ -0,0 +1,52 @@
+-- Create a normalized table
+CREATE TABLE ProductDetail_1NF (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Product VARCHAR(50)
+);
+
+-- Insert normalized data
+INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+
+
+-- Q2
+-- Create a separate table for Order information
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+-- Create a table for Order Details (Product & Quantity)
+CREATE TABLE OrderDetails_2NF (
+    OrderID INT,
+    Product VARCHAR(50),
+    Quantity INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+-- Insert data into Orders table
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+-- Insert data into OrderDetails_2NF table
+INSERT INTO OrderDetails_2NF (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
+
+
